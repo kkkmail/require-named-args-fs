8e69958de1dbe818b90c64c7084e0e7f6e0ff973
kkkmail <kkk@jfcglobal.net>
2020-03-10 10:00:40 -0400

    Initial commit

---
+++ RequireNamedArgs/Analysis/ParameterInfo.fs
@@ -0,0 +1,61 @@
+﻿module RequireNamedArgs.ParameterInfo
+
+open System
+open System.Collections.Immutable
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open RequireNamedArgs.CSharpAdapters
+open RequireNamedArgs.MaybeBuilder
+
+type ParameterInfo = {
+    MethodOrProperty : ISymbol;
+    Parameter : IParameterSymbol }
+
+type ISymbol with
+    member symbol.GetParameters() =
+        match symbol with
+        | :? IMethodSymbol as s   -> s.Parameters
+        | :? IPropertySymbol as s -> s.Parameters
+        | _                       -> ImmutableArray<IParameterSymbol>.Empty
+        |> Seq.toList
+
+/// <summary>
+/// To be able to convert positional arguments to named we need to find
+/// corresponding <see cref="IParameterSymbol" /> for each argument.
+/// </summary>
+type SemanticModel with
+    member sema.GetParameterInfo (argument: ArgumentSyntax) = maybe {
+        let argList = argument.Parent :?> ArgumentListSyntax
+        let exprSyntax = argList.Parent  :?> ExpressionSyntax
+        let methodOrProperty = sema.GetSymbolInfo(exprSyntax).Symbol
+
+        let parameters = methodOrProperty.GetParameters()
+        if parameters.IsEmpty
+        then return! None
+        else
+
+        if isNull argument.NameColon 
+        then
+            // A positional argument.
+            match argList.Arguments.IndexOf(argument) with
+            | index when index >= 0 && index < parameters.Length -> 
+                return { MethodOrProperty = methodOrProperty;
+                         Parameter = parameters.[index] }
+            | index when index >= parameters.Length 
+                            && parameters.[parameters.Length - 1].IsParams ->
+                return { MethodOrProperty = methodOrProperty;
+                         Parameter = parameters.[parameters.Length - 1] }
+            | _ -> return! None
+        else 
+            // Potentially, this is a named argument.
+            let! name = argument.NameColon.Name |> Option.ofObj
+            let! nameText = name.Identifier.ValueText |> Option.ofObj
+            // Yes, it's a named argument.
+            let! parameter = parameters |> Seq.tryFind (fun param -> 
+                String.Equals(param.Name, 
+                              nameText, 
+                              StringComparison.Ordinal))
+
+            return { MethodOrProperty = methodOrProperty;
+                     Parameter = parameter }
+    }
---
+++ RequireNamedArgs/RequireNamedArgsAnalyzer.fs
@@ -0,0 +1,83 @@
+namespace RequireNamedArgs.Analyzer
+
+open System
+open System.Collections.Immutable
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open Microsoft.CodeAnalysis.Diagnostics
+open RequireNamedArgs.ArgumentAndParameter // Actually it's used
+open RequireNamedArgs.CSharpAdapters
+open RequireNamedArgs.InvocationExprSyntax
+open RequireNamedArgs.MaybeBuilder
+
+[<DiagnosticAnalyzer(Microsoft.CodeAnalysis.LanguageNames.CSharp)>]
+type public RequireNamedArgsAnalyzer() = 
+    inherit DiagnosticAnalyzer()
+
+    static let diagnosticId = "RequireNamedArgs"
+    static let messageFormat = "A [RequireNamedArgs] method '{0}' must be invoked with named arguments"
+    static let description = "Methods marked with `[RequireNamedArgs]` must be invoked with named arguments."
+    static let descriptor = 
+        DiagnosticDescriptor(
+            id=diagnosticId,
+            title="[RequireNamedArgs] method invocation with positional arguments.",
+            messageFormat=messageFormat,
+            category="Code style",
+            defaultSeverity=DiagnosticSeverity.Error, 
+            isEnabledByDefault=true, 
+            description=description,
+            helpLinkUri=null)
+
+    static member DiagnosticId = diagnosticId
+    static member MessageFormat = messageFormat
+
+    override val SupportedDiagnostics = ImmutableArray.Create(descriptor)
+
+    override this.Initialize (context: AnalysisContext) =
+        // Register ourself to get invoked to analyze 
+        //   - invocation expressions; e. g., calling a method. 
+        //   - and object creation expressions; e. g., invoking a constructor.
+        context.RegisterSyntaxNodeAction(
+            (fun c -> this.Analyze c),
+            SyntaxKind.InvocationExpression, 
+            SyntaxKind.ObjectCreationExpression)
+
+    member private this.filterSupported (methodSymbol: IMethodSymbol) = 
+        match methodSymbol.MethodKind with
+        // So far we only support analyzing of the three kinds of methods listed below.
+        |     MethodKind.Ordinary
+            | MethodKind.Constructor 
+            | MethodKind.LocalFunction -> Some methodSymbol
+        | _                            -> None
+
+    member private this.formatDiagMessage argsWhichShouldBeNamed =
+        String.Join(
+            ", ",
+            argsWhichShouldBeNamed |> Seq.map (fun it -> sprintf "'%s'" it.Parameter.Name))
+
+    member private this.Analyze(context: SyntaxNodeAnalysisContext) =
+        maybe {
+            let! invocationExprSyntax = context.Node |> Option.ofType<InvocationExpressionSyntax>
+            let! methodSymbol = context.SemanticModel.GetSymbolInfo(invocationExprSyntax).Symbol 
+                                |> Option.ofType<IMethodSymbol>
+            let! methodSymbol = methodSymbol |> this.filterSupported
+            // We got a supported kind of method.
+            // Delegate heavy-lifting to the call below.
+            let! argsWhichShouldBeNamed = getArgsWhichShouldBeNamed context.SemanticModel 
+                                                                    invocationExprSyntax
+
+            // We inspected the arguments of invocation expression.
+            if argsWhichShouldBeNamed |> Seq.any 
+            then // There are arguments that should be named -- emit the diagnostic.
+                 return context.ReportDiagnostic(
+                     Diagnostic.Create(
+                         descriptor, 
+                         invocationExprSyntax.GetLocation(),
+                         // messageArgs
+                         methodSymbol.Name, 
+                         this.formatDiagMessage argsWhichShouldBeNamed))
+            // If none of them should be named or, maybe, they already are named,
+            // we have nothing more to do.
+            else return ()
+        } |> ignore
